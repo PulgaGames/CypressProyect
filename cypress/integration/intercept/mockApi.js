973506d87c27f740753d5fde8bde3775872ec8bb
JoanEsquivel <48256183+JoanEsquivel@users.noreply.github.com>
2021-05-20 14:40:41 -0700

    intercept demo

--- cypress/integration/intercept/mockApi.js
+++ cypress/integration/intercept/mockApi.js
@@ -2,47 +2,50 @@
 //Mock API responses may be useful during development and testing when live data is either unavailable or unreliable.
 // While designing an API, you can use mock APIs to work concurrently on the front and back-end, as well as to gather feedback from developers. 
 
-describe('Intercept Demo', function(){
+describe('Intercept Demo', () => {
 
-    it('Initial Validation', function(){
-        cy.visit('http://localhost:3000/');
+    it('Intitial Validation', () => {
+        cy.visit('http://localhost:3000/')
         cy.get('#todo-list li')
         .should('have.length', 2)
         .and('contain', 'test')
         .and('contain', 'wash dishes')
     });
 
-    it('Mocked API Response validation',function(){
+    it('Mocked API Response', ()=> {
 
-        cy.intercept('GET' , '/todos', {fixture: 'intercept/interceptFixture.json'}).as('getTodos-fixture');
+        cy.intercept('GET', '/todos', {
+            fixture: 'intercept/interceptFixture.json'
+        }).as('getTodos-Fixture')
 
-        cy.visit('http://localhost:3000/');
+        cy.visit('http://localhost:3000/')
 
         cy.get('#todo-list li')
         .should('have.length', 3)
-        .and('contain', 'Cypress assertions video')
+        .and('contain', 'Cypress Assertions Video')
         .and('contain', 'Page Object Model Cypress')
         .and('contain', 'Intercept Cypress')
-
+    
     })
 
-    it('Mocked a ready todo list', function(){
-
-        const stubSample = [{
-            "title": "Test Intercept",
-            "completed": true,
-            "id": "1"
+    it('Mocked a ready todo item', () => {
+        
+        const stubExample = [{
+            "title": "Mock API Response", 
+            "completed": true, 
+            "id":1
         }]
 
-        cy.intercept('GET' , '/todos', {body: stubSample}).as('getTodos-body');
+        cy.intercept('GET', '/todos', {
+            body: stubExample
+        })
 
-        cy.visit('http://localhost:3000/');
+        cy.visit('http://localhost:3000/')
 
         cy.get('div label').should('have.css', 'text-decoration', 'line-through solid rgb(217, 217, 217)')
 
-    })
+    });
 
 
-
-
-})
+    
+});
